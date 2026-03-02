b1dcd1a9efd7151504dd0c3fe290ae8b23b79772
sarahsiqueira777 <anakelleycarril@gmail.com>
2026-03-02 16:50:36 -0400

    the database call header configuration have been completed

--- src/banco.php
+++ src/banco.php
@@ -5,13 +5,19 @@ $senha = "";
 $banco = "estoque";
 
 try {
-    //code...
-    $conexao = new PDO("
-    mysql:host=$servidor;
-    dbname=$banco;
-    charset=utf8",
-    $usuario,
-    $senha);
-} catch (\Throwable $th) {
-    //throw $th;
+    //tentativa da conexão com banco de dados
+    //código...
+    //classe PDO é usada para acessar vários tipos de banco de dados usando a mesma conexão
+    /*DSN DATA NAME é uma string de conexão usada para especificar o tipo de banco de dados 
+    o host e nme do banco de dados*/  
+    $conexao = new PDO("mysql:host=$servidor; dbname=$banco; charset=utf8", $usuario, $senha);
+    $conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo"Conexão com banco de dados estabelecida com sucesso!";
+    //echo utlizadp para printar algo na tela
+
+} catch (\Throwable $erro) {
+    //lançavel serve para qualquer tipo de erro ou exceção
+    //captura de errro caso a conexão falhe
+    die("Erro ao conectar com banco de dados:". $erro->getMessage());
+
 }
